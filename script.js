d20ef63977e687455fe41f1a1f42df07bf5e2320
abdifatahabdiladifahmed <abdifatahabdiladifahmed@gmail.com>
2024-11-06 21:57:23 +0300

    changes

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const menuItems = [
-  { title: "Dish Menu 1", image: "images/2.jpg" },
-  { title: "Dish Menu 2", image: "images/11.jpg" },
-  { title: "Dish Menu 3", image: "images/15.jpg" },
-  { title: "Dish Menu 4", image: "images/18.jpg" },
-  { title: "Dish Menu 5", image: "images/21.jpg" },
-  { title: "Dish Menu 6", image: "images/22.jpg" },
-  { title: "Dish Menu 7", image: "images/23.jpg" },
-  { title: "Dish Menu 8", image: "images/24.jpg" }
+  { title: "Dish Menu 1", image: "Public/images/2.jpg" },
+  { title: "Dish Menu 2", image: "Public/images/11.jpg" },
+  { title: "Dish Menu 3", image: "Public/images/15.jpg" },
+  { title: "Dish Menu 4", image: "Public/images/18.jpg" },
+  { title: "Dish Menu 5", image: "Public/images/21.jpg" },
+  { title: "Dish Menu 6", image: "Public/images/22.jpg" },
+  { title: "Dish Menu 7", image: "Public/images/23.jpg" },
+  { title: "Dish Menu 8", image: "Public/images/24.jpg" }
 ];
 
 const loadMenuItems = () => {
