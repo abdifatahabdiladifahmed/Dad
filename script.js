05a5fad3d8a7438a110fafaeeb26b410b89c8d3f
abdifatahabdiladifahmed <abdifatahabdiladifahmed@gmail.com>
2024-11-06 22:09:57 +0300

    c

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-const menuItems = [
-  { title: "Dish Menu 1", image: "Public/images/2.jpg" },
-  { title: "Dish Menu 2", image: "Public/images/11.jpg" },
-  { title: "Dish Menu 3", image: "Public/images/15.jpg" },
-  { title: "Dish Menu 4", image: "Public/images/18.jpg" },
-  { title: "Dish Menu 5", image: "Public/images/21.jpg" },
-  { title: "Dish Menu 6", image: "Public/images/22.jpg" },
-  { title: "Dish Menu 7", image: "Public/images/23.jpg" },
-  { title: "Dish Menu 8", image: "Public/images/24.jpg" }
-];
+// const menuItems = [
+//   { title: "Dish Menu 1", image: "images/2.jpg" },
+//   { title: "Dish Menu 2", image: "images/11.jpg" },
+//   { title: "Dish Menu 3", image: "images/15.jpg" },
+//   { title: "Dish Menu 4", image: "images/18.jpg" },
+//   { title: "Dish Menu 5", image: "images/21.jpg" },
+//   { title: "Dish Menu 6", image: "images/22.jpg" },
+//   { title: "Dish Menu 7", image: "images/23.jpg" },
+//   { title: "Dish Menu 8", image: "images/24.jpg" }
+// ];
 
-const loadMenuItems = () => {
-  const menuContainer = document.getElementById("menu-items");
-  menuContainer.innerHTML = "";
+// const loadMenuItems = () => {
+//   const menuContainer = document.getElementById("menu-items");
+//   menuContainer.innerHTML = "";
 
-  menuItems.forEach(item => {
-    const menuItem = document.createElement("div");
-    menuItem.classList.add("menu-item", "bg-white", "rounded", "shadow-md", "p-6");
+//   menuItems.forEach(item => {
+//     const menuItem = document.createElement("div");
+//     menuItem.classList.add("menu-item", "bg-white", "rounded", "shadow-md", "p-6");
 
-    menuItem.innerHTML = `
-      <img src="${item.image}" alt="${item.title}" class="w-full object-cover rounded mb-6">
-      <h3 class="text-2xl font-bold mb-4">${item.title}</h3>
-    `;
+//     menuItem.innerHTML = `
+//       <img src="${item.image}" alt="${item.title}" class="w-full object-cover rounded mb-6">
+//       <h3 class="text-2xl font-bold mb-4">${item.title}</h3>
+//     `;
 
-    menuContainer.appendChild(menuItem);
-  });
-};
+//     menuContainer.appendChild(menuItem);
+//   });
+// };
 
-document.addEventListener("DOMContentLoaded", loadMenuItems);
+// document.addEventListener("DOMContentLoaded", loadMenuItems);
